84823abab406ce9fa66e33cc828ee9e58327e5ae
Dmitry Bessonov <bessonov.dmitry@gmail.com>
2022-11-05 16:28:48 +0300

    test_13

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 DEBUG = True
 
 ALLOWED_HOSTS = ['practicum.bessonovda.ru',
-                 '178.154.200.51'
-                ]
+                 '178.154.200.51']
 
 
 # Application definition
